a0701d5199cdf6929613f6f107a9715b9f73619e
Daniel Robert <Daniel.Robert.S-S@hotmail.com>
2021-04-14 11:23:05 +1000

    added circle

---
+++ Primitives/Shapes/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uaine.Objects.Primitives.Shapes
+{
+    public class Circle : Shape
+    {
+        public float radius;
+
+        public Circle(float r)
+        {
+            radius = r;
+        }
+
+        public override float Measure()
+        {
+            return (float)(radius * radius * Math.PI);
+        }
+    }
+}
