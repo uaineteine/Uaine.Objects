7faa13d127c2a83a60bb90a58c173f6a7581f66c
Daniel Robert <Daniel.Robert.S-S@hotmail.com>
2021-05-14 21:13:49 +1000

    msg and not

---
+++ Primitives/Message.cs
@@ -0,0 +1,18 @@
+﻿namespace Uaine.Objects.Primitives
+{
+    public class Message
+    {
+        public TextObject Title;
+        public TextObject Description;
+        public Message(TextObject title)
+        {
+            Title = title;
+            Description = new TextObject("");
+        }
+        public Message(TextObject title, TextObject desc)
+        {
+            Title = title;
+            Description = desc;
+        }
+    }
+}
---
+++ Primitives/Notification.cs
@@ -0,0 +1,13 @@
+﻿namespace Uaine.Objects.Primitives
+{
+    public class Notification
+    {
+        public Message message;
+        public float NotificationTime { get; set; }
+        public Notification(Message msg, float time)
+        {
+            message = msg;
+            NotificationTime = time;
+        }
+    }
+}
