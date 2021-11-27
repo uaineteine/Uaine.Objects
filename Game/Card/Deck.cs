77699fa95ed88f8db23febd151cc4b8f4a21cc85
Daniel Robert <Daniel.Robert.S-S@hotmail.com>
2021-11-27 13:52:53 +1100

    increased card functionality

--- Game/Card/Deck.cs
+++ Game/Card/Deck.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Uaine.Objects.Primitives.Values;
 
@@ -7,24 +6,22 @@ namespace Uaine.Objects.Game.Card
 {
     public class Deck
     {
-        protected IValue numCards;
-        public int RemainingCards { get => numCards.Value; }
-        public int DeckSize { get => numCards.def; }
-        protected Card[] cardDeck;
+        protected List<Card> cardDeck;
         protected List<Card> discardPile;
+        public int RemainingCards { get => cardDeck.Count; }
         public int NumberDiscardCards { get => discardPile.Count; }
 
         public Deck(int numValues, int numClasses)
         {
-            numCards = numValues * numClasses;
-            cardDeck = new Card[numCards.Value];
+            //int size = numValues * numClasses;
+            cardDeck = new List<Card>();
             discardPile = new List<Card>();
             int k = 0;
             for (int i = 0; i < numValues; i++)
             {
                 for (int c = 0; c < numClasses; c++)
                 {
-                    cardDeck[k] = new Card(i, c);
+                    cardDeck.Add(new Card(i, c));
                     k += 1;
                 }
             }
@@ -34,7 +31,23 @@ namespace Uaine.Objects.Game.Card
         public void ShuffleDeck()
         {
             System.Random random = new System.Random();
-            cardDeck = cardDeck.OrderBy(x => random.Next()).ToArray();
+            cardDeck = new List<Card>(cardDeck.OrderBy(x => random.Next()).ToArray());
+        }
+
+        public Card getTopMostCard()
+        {
+            return cardDeck[RemainingCards - 1];
+        }
+        public Card getRandomCard()
+        {
+            System.Random random = new System.Random();
+            int i = random.Next(RemainingCards);
+            return cardDeck[i];
+        }
+
+        public void addToDiscardPile(Card thisCard)
+        {
+            discardPile.Add(thisCard);
         }
     }
 }
