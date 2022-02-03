d0513e34c6c4a90e3ba3e07c66a17805bf88b1a7
Daniel Robert <Daniel.Robert.S-S@hotmail.com>
2022-02-04 00:03:16 +1100

    1.9.3 release with operators for Card PolyIDs have blank top line removed

--- Game/Card/Card.cs
+++ Game/Card/Card.cs
@@ -12,5 +12,29 @@ namespace Uaine.Objects.Game.Card
             cardValue = value;
             cardClass = cardclass;
         }
+
+        //OPERATORS
+        public static bool operator ==(Card lhs, Card rhs)
+        {
+            if (lhs is null)
+            {
+                if (rhs is null)
+                {
+                    // null == null = true.
+                    return true;
+                }
+
+                // Only the left side is null.
+                return false;
+            }
+            // Equals handles the case of null on right side.
+            if (lhs.cardValue == rhs.cardValue)
+            {
+                if (lhs.cardClass == rhs.cardClass)
+                    return true;
+            }
+            return false; //else
+        }
+        public static bool operator !=(Card lhs, Card rhs) => !(lhs == rhs);
     }
 }
--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Base classes to inherit from:
 
 ![IDDiagram](https://raw.githubusercontent.com/uaineteine/Uaine.Objects/release/images/IDs.png)
 
-## Version 1.9.2
+## Version 1.9.3
 
 See the [changelog](changelog.txt) for details.
 
