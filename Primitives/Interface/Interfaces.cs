9ce41c056012cd8ad31530993dcfa8a882c799e9
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2023-03-10 00:14:09 +1100

    add interface for operations

---
+++ Primitives/Interface/Interfaces.cs
@@ -0,0 +1,11 @@
+﻿namespace Uaine.Objects.Primitives.Interface
+{
+    public interface IArithmetic<T>
+    {
+        T Add(T other);
+        T Subtract(T other);
+        T Multiply(T other);
+        T Divide(T other);
+    }
+
+}
