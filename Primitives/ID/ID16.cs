a2ac971428772cdd4b86f5a1096a8bc0ec0566db
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-08-24 14:22:01 +1000

    --3.1-- ID classes include hashing functions and == != operators

--- Primitives/ID/ID16.cs
+++ Primitives/ID/ID16.cs
@@ -14,11 +14,49 @@ namespace Uaine.Objects.Primitives.ID
         public static implicit operator ID32(ID16 obj) => new ID32(obj);
         public static implicit operator short(ID16 obj) => obj.ID;
         public static implicit operator ID16(short id) => new ID16(id);
+        public static bool operator ==(ID16 left, ID16 right)
+        {
+            if (ReferenceEquals(left, right))
+            {
+                return true;
+            }
+            if (left is null || right is null)
+            {
+                return false;
+            }
+            return left._id == right._id;
+        }
+        public static bool operator !=(ID16 left, ID16 right)
+        {
+            return !(left == right);
+        }
+        public static bool operator ==(ID16 left, short right)
+        {
+            return left == new ID16(right);
+        }
+        public static bool operator !=(ID16 left, short right)
+        {
+            return left != new ID16(right);
+        }
+        public static bool operator ==(short left, ID16 right)
+        {
+            return right == new ID16(left);
+        }
+        public static bool operator !=(short left, ID16 right)
+        {
+            return right != new ID16(left);
+        }
 
         //overrides
         public override string ToString()
         {
             return _id.ToString();
+        }
+
+
+        public override int GetHashCode()
+        {
+            return _id.GetHashCode();
         }
     }
 }
--- Primitives/ID/ID64.cs
+++ Primitives/ID/ID64.cs
@@ -13,11 +13,49 @@ namespace Uaine.Objects.Primitives.ID
         //operators
         public static implicit operator long(ID64 obj) => obj.ID;
         public static implicit operator ID64(long id) => new ID64(id);
+        public static bool operator ==(ID64 left, ID64 right)
+        {
+            if (ReferenceEquals(left, right))
+            {
+                return true;
+            }
+            if (left is null || right is null)
+            {
+                return false;
+            }
+            return left._id == right._id;
+        }
+        public static bool operator !=(ID64 left, ID64 right)
+        {
+            return !(left == right);
+        }
+        public static bool operator ==(ID64 left, long right)
+        {
+            return left == new ID64(right);
+        }
+        public static bool operator !=(ID64 left, long right)
+        {
+            return left != new ID64(right);
+        }
+        public static bool operator ==(long left, ID64 right)
+        {
+            return right == new ID64(left);
+        }
+        public static bool operator !=(long left, ID64 right)
+        {
+            return right != new ID64(left);
+        }
+
 
         //overrides
         public override string ToString()
         {
             return _id.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            return _id.GetHashCode();
         }
     }
 }
