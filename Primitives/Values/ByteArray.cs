cae8b007a23c366262e10326dc85e32118ff6d0f
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-08-24 14:41:28 +1000

    ID classes include hashing functions and == != operators ByteArray value as a base class added and used to simplify hash code object

---
+++ Primitives/Values/ByteArray.cs
@@ -0,0 +1,46 @@
+﻿
+using Newtonsoft.Json;
+using System.Collections;
+using System;
+
+namespace Uaine.Objects.Primitives.Values
+{
+    public class ByteArray
+    {
+        public byte[] bytes { get; }
+
+
+        public ByteArray(byte[] byts)
+        {
+            bytes = byts;
+        }
+
+        public bool Equals(ByteArray obj)
+        {
+            return bytes.Equals(obj.bytes);
+        }
+
+        public static bool operator ==(ByteArray left, ByteArray right)
+        {
+            if (ReferenceEquals(left, null))
+                return ReferenceEquals(right, null);
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(ByteArray left, ByteArray right)
+        {
+            return !(left == right);
+        }
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public static ByteArray FromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<ByteArray>(json);
+        }
+    }
+}
